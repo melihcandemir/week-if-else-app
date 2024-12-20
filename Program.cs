13a608bd33134b3ebb787aad1b013fbf80de0d4b
melih can <melihcanndemir@gmail.com>
2024-12-20 19:17:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+// kullanıcıdan sayı alıyoruz
+Console.Write("Sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+// sayının 10 ile olan durumunu belirliyoruz
+if (sayi > 10) 
+{
+    // tek veya çift olmasını kontrol ediyoruz
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine($"{sayi} sayısı 10'dan büyük ve çift sayıdır");
+    }
+    else
+    {
+        Console.WriteLine($"{sayi} sayısı 10'dan büyük ve tek sayıdır");
+    }
+}
+else if (sayi < 10)
+{
+    // tek veya çift olmasını kontrol ediyoruz
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine($"{sayi} sayısı 10'dan küçük ve çift sayıdır");
+    }
+    else
+    {
+        Console.WriteLine($"{sayi} sayısı 10'dan küçük ve tek sayıdır");
+    }
+}
+else // bu koşullar olmaması durumda sayı 10 ve çift sayıdır
+{
+    Console.WriteLine("Sayı 10'a eşit ve çift sayıdır");
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Week if else
+Dersde öğrendiğimiz if else konusu ile ilgili uygulama
+
+## Kod:
+![](img/code.png)
+
+## Output:
+![](img/output.png)
